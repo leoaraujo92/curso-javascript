3196b1f5e5eeb5497d7d46138b8a35dae3d00370
leoaraujo92 <leonardo.araujo2008@gmail.com>
2025-10-30 20:43:05 -0300

    odulo 2 52

--- MODULO2/52.for-classico/app.js
+++ MODULO2/52.for-classico/app.js
@@ -1,5 +1,15 @@
 // for classico
 
 for (let i = 0; i <= 5; i++){ // i variável de controle (i) index
-    console.log(`Linha ${i}`)
+    //console.log(`Linha ${i}`)
 }
+
+//percorrer array
+
+const frutas = ['Abacate', 'Banana', 'Caqui', 'Damasco', 'Figo'];
+
+
+for (let i = 0; i < frutas.length; i++){
+    console.log(frutas[i])
+}
+
